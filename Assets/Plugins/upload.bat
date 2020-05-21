e67c5753d1c24038bd83e5973d85f6d5e331afe5
UnMapache <josuecoriaxp@gmail.com>
2020-05-20 20:55:42 -0500

    Polygon Assets

---
+++ Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@D:\Proyectos\Aplicaciones Unity\Freelance\AltSpace\VT_DEMO\template.zip" -F "space_template[game_engine_version]=20192" https://account.altvr.com/api/space_templates/1473670007916331562.json
